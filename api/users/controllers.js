769f8870858618d89cb5d0987daadd61e626f5d3
q8wahab <q8_xprt@hotmail.com>
2024-07-02 18:44:47 +0300

    add the logout button

--- api/users/controllers.js
+++ api/users/controllers.js
@@ -15,6 +15,8 @@ const generateToken = (user) => {
 const signup = async (req, res, next) => {
   try {
     //hash the password
+
+    console.log("body", req.body);
     const salt = await bcrypt.genSalt(10);
     const password = await bcrypt.hash(req.body.password, salt);
     req.body.password = password;
